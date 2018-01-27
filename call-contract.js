194adeccefda600ee5b3131f780d391f1a32f563
makevoid <makevoid@gmail.com>
2018-01-27 02:40:00 +0000

    fix call contract

--- call-contract.js
+++ call-contract.js
@@ -1,3 +1,5 @@
+require('isomorphic-fetch')
+require('isomorphic-form-data')
 const Web3 = require('web3')
 const fs = require('fs')
 const web3 = new Web3('https://kovan.infura.io')
@@ -11,7 +13,24 @@ const privateKey = readFileSync(keyFile)
 const account = eth.accounts.privateKeyToAccount(privateKey)
 const address = account.address
 const abi = require("./contract.abi.json")
-const toWei = web3.utils.toWei
+const wUtils    = web3.utils
+const toWei     = wUtils.toWei
+const hexToUtf8 = wUtils.hexToUtf8
+const utf8ToHex = wUtils.utf8ToHex
+
+const broadcastTransaction = async (rawTx) => {
+  const broadcastUrl = "https://kovan.etherscan.io/api"
+  const data = new FormData()
+  data.append('module', 'proxy')
+  data.append('action', 'eth_sendRawTransaction')
+  data.append('hex', rawTx)
+  let resp = await fetch(broadcastUrl, {
+    method: "post",
+    body: data,
+  })
+  resp = await resp.json()
+  return resp
+}
 
 if (!existsSync(contractAddressFile)) {
   console.error("Error: contract address not present, deploy your contract first! exiting...")
@@ -32,42 +51,41 @@ const contractAddress = readFileSync(contractAddressFile)
   }
 
   try {
-    const ctrAddress = web3.utils.toChecksumAddress(contractAddress.toString())
-    console.log(ctrAddress)
-    const ctrOptions = {
-      gasPrice: toWei("6", "gwei"),
-      chainId: "0x2A", // Kovan chain (id: 42)
-    }
-    const contractClass = new web3.eth.Contract(abi, ctrAddress, ctrOptions)
-    contractClass.options.address = ctrAddress
+    const ctrAddress = web3.utils.toChecksumAddress(contractAddress.toString().trim())
+
+    const contractClass = new web3.eth.Contract(abi, ctrAddress)
     const contractInstance = contractClass.methods
 
+    // call getter
     const getter    = contractInstance.data()
     const getterRes = await getter.call()
+    console.log("Getter: ", hexToUtf8(getterRes))
 
-    console.log("Getter: ", getterRes)
-    // --------
-
-
-    // getter
+    // setter
     const rand = Math.round(Math.random()*999)
-    const setterCall  = contractInstance.set.send(`foo-${rand}`)
+    const value = utf8ToHex(`foo-${rand}`)
+    const setterCall  = contractInstance.set(value)
     const setterAbi   = setterCall.encodeABI()
-    let getterGas = await contractInstance.data().estimateGas()
-    console.log("gas", getterGas)
-    //
-    // const getterData = {
-    //   chainId: "0x2A", // Kovan chain (id: 42)
-    //   from: address,
-    //   data: getterAbi,
-    //   gas:  getterGas,
-    //   gasPrice: toWei("6", "gwei"),
-    //   // nonce: 1,
-    // }
-    //
-    // const tx = await account.signTransaction(getterData)
-    // const txRaw = tx.rawTransaction
-    // console.log("Raw TX:", txRaw)
+    let setterGas = await contractInstance.data().estimateGas()
+    setterGas = Math.max(265056, setterGas)
+    console.log("gas:", setterGas)
+
+    const setterData = {
+      chainId: "0x2A", // Kovan chain (id: 42)
+      from: address,
+      data: setterAbi,
+      gas:  setterGas,
+      gasPrice: toWei("6", "gwei"),
+      to: ctrAddress,
+      // nonce: 1,
+    }
+
+    const tx = await account.signTransaction(setterData)
+    const txRaw = tx.rawTransaction
+    console.log("Raw TX:", txRaw)
+
+    const response = await broadcastTransaction(txRaw)
+    console.log("PUSH TX:", response)
 
   } catch (err) {
     console.error("Error", err)
